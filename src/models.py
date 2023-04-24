4fe242e1bf1e63efc36a300a832d9a5175900fe1
Guillaume COGONI <cogoni.guillaume@gmail.com>
2023-04-24 11:13:35 +0200

    [Refactoring]
    Add GRU model.
    Change the train_LSTM to train_model.
    
    Signed-off-by: Guillaume COGONI <cogoni.guillaume@gmail.com>
    Co-Authored : michaelbenalipro@gmail.com

--- src/models.py
+++ src/models.py
@@ -21,10 +21,26 @@ class LSTMModel(torch.nn.Module):
         out = self.fc(out[:, -1, :])
         return out
 
+class GRUModel(torch.nn.Module):
+    """
+    Class to define GRU model here with 6 GRU layers and 1 fully connected layer by default
+    """
+    def __init__(self, input_size, hidden_size, output_size, num_layers=6):
+        super(GRUModel, self).__init__()
+        self.hidden_size = hidden_size
+        self.num_layers = num_layers
+        
+        self.gru = torch.nn.GRU(input_size, hidden_size, num_layers, batch_first=True)
+        self.fc = torch.nn.Linear(hidden_size, output_size)
+        
+    def forward(self, x):
+        out, _ = self.gru(x)
+        out = self.fc(out[:, -1, :])
+        return out
 
 
 import os
-def train_LSTM(model,train_loader,val_loader, num_epochs = 200):
+def train_model(model,train_loader,val_loader, num_epochs = 200, model_path = './'):
     # Train your model and evaluate on the validation set
     # Define the loss function and optimizer
     criterion = torch.nn.MSELoss()
@@ -57,11 +73,11 @@ def train_LSTM(model,train_loader,val_loader, num_epochs = 200):
 
         if val_loss < best_val_loss:
             best_val_loss = val_loss
-            torch.save(model.state_dict(), 'best_model.pth')
+            torch.save(model.state_dict(), model_path + 'best_model.pth')
         print(f'Epoch {epoch+1}/{num_epochs}, Training Loss: {train_loss:.4f}, Validation Loss: {val_loss:.4f}')
     best_model =  copy.deepcopy(model)
-    best_model.load_state_dict(torch.load('best_model.pth'))
-    os.remove("./best_model.pth")
+    best_model.load_state_dict(torch.load(model_path +'best_model.pth'))
+    os.remove(model_path + "best_model.pth")
     return best_model
 
 
