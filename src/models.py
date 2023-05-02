Decoupling Test Model Separation of modular function of test, plot vs predict and metric calculation. Corrected some missing and wrong package in utils_data.py and models.py

src/models.py
@@ -34,7 +34,7 @@ class LSTMModel(torch.nn.Module):
     num_layer : int = 6
         number of layer.
     """
-    def __init__(self, input_size : int, hidden_size : int, output_size : int, num_layers : int=6):
+    def __init__(self, input_size : int,  output_size : int, hidden_size : int =32, num_layers : int=6):
         super().__init__()
         self.lstm = torch.nn.LSTM(input_size, hidden_size, num_layers, batch_first=True)
         self.fc = torch.nn.Linear(hidden_size, output_size)
@@ -63,7 +63,7 @@ class GRUModel(torch.nn.Module):
         number of layer.
     """
     
-    def __init__(self, input_size : int, hidden_size : int, output_size : int, num_layers : int=6):
+    def __init__(self, input_size : int,  output_size : int, hidden_size : int=32, num_layers : int=6):
         super(GRUModel, self).__init__()
         self.hidden_size = hidden_size
         self.num_layers = num_layers
@@ -337,13 +337,13 @@ def train_model(model, train_loader, val_loader, model_path, num_epochs = 200, r
     best_model.load_state_dict(torch.load(model_path))
     if remove:
         os.remove(model_path)
-    return best_model
+    return best_model, train_losses, valid_losses
 
 
-def testmodel(best_model,test_loader, path='local.pth', plot =False, criterion = torch.nn.MSELoss(), percentage_error_fix = 1):
-    from src.metrics import rmse, rmspe, maape, mape 
-    from sklearn.metrics import mean_absolute_error
-
+def testmodel(best_model, test_loader, path=None, criterion = torch.nn.MSELoss()):
+    
+    import numpy as np
+    
     """
     Test model using test data
 
@@ -358,10 +358,6 @@ def testmodel(best_model,test_loader, path='local.pth', plot =False, criterion =
     path : string
         model path to load the model from
 
-    plot : bool=False
-        plot actual vs prediction
-    percentage_error_fix : float
-        Add a float to the time serie for calculation of percentage because of null values 
     
     Returns
     ----------
@@ -370,7 +366,42 @@ def testmodel(best_model,test_loader, path='local.pth', plot =False, criterion =
 
     y_true : array
         actual values to compare to the prediction
-    metric_dict : dictionary
+    """
+
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    if path :
+        best_model.load_state_dict(torch.load(path))
+    best_model = best_model.to(device)
+    best_model.double()
+    best_model.eval()
+    predictions = []
+    actuals = []
+    with torch.no_grad():
+        for i, (inputs, targets) in enumerate(test_loader):
+            inputs = inputs.to(device)
+            targets = targets.to(device)
+            x = torch.Tensor(inputs).unsqueeze(1).to(device)
+            y = torch.Tensor(targets).unsqueeze(0).to(device)
+            outputs = best_model(inputs)
+            predictions.append(outputs.cpu().numpy())
+            actuals.append(targets.cpu().numpy())
+    predictions = np.concatenate(predictions, axis=0)
+    actuals = np.concatenate(actuals, axis=0)
+    
+    y_pred = predictions[:]
+    y_true = actuals[:]
+    
+    return y_true, y_pred
+
+def calculate_metrics(y_true, y_pred,percentage_error_fix =0):
+    from src.metrics import rmse, rmspe, maape, mape 
+    from sklearn.metrics import mean_absolute_error
+    
+    """
+    percentage_error_fix : float
+        Add a float to the time serie for calculation of percentage because of null values 
+
+        metric_dict : dictionary
         Contain the following metrics : 
         rmse_val: 
             Root mean square error calculate between y_pred and y_true
@@ -383,48 +414,30 @@ def testmodel(best_model,test_loader, path='local.pth', plot =False, criterion =
         maape_val:
             Mean Arctangente percentage error calculate between y_pred and y_true
     """
+    metric_dict={}
+    for i in range(len(y_pred[0,:])):
+        rmse_val= rmse(y_true[i],y_pred[i])
+        rmspe_val = rmspe(y_true[i],y_pred[i],percentage_error_fix)
+        mae_val = mean_absolute_error(y_true[i],y_pred[i])
+        mape_val = mape(y_true[i],y_pred[i],percentage_error_fix)
+        maape_val =  maape(y_true[i],y_pred[i],percentage_error_fix)
+        if len(y_pred[0,:]) ==1 :
+            metric_dict = {"RMSE":rmse_val, "RMSPE": rmspe_val, "MAE":mae_val,"MAPE":mape_val, "MAAPE": maape_val}
+        else:
+            metric_dict[i] = {"RMSE":rmse_val, "RMSPE": rmspe_val, "MAE":mae_val,"MAPE":mape_val, "MAAPE": maape_val}
+    return metric_dict
 
-    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-    best_model.load_state_dict(torch.load(path))
-    best_model = best_model.to(device)
-    best_model.double()
-    best_model.eval()
-    test_loss = 0.0
-    predictions = []
-    actuals = []
-    # test_data = datadict['test_data']
-    with torch.no_grad():
-        for i, (inputs, targets) in enumerate(test_loader):
-            inputs = inputs.to(device)
-            targets = targets.to(device)
-            x = torch.Tensor(inputs).unsqueeze(1).to(device)
-            y = torch.Tensor(targets).unsqueeze(0).to(device)
-            outputs = best_model(inputs)
-            loss = criterion(outputs, targets)
-            test_loss += loss.item()
-            predictions.append(outputs.cpu().numpy())
-            actuals.append(targets.cpu().numpy())
-    test_loss /= len(test_loader)
-    predictions = np.concatenate(predictions, axis=0)
-    actuals = np.concatenate(actuals, axis=0)
-    
-    y_pred = predictions[:,0]
-    y_true = actuals[:,0]
-    rmse_val= rmse(y_true,y_pred)
-    rmspe_val = rmspe(y_true,y_pred,percentage_error_fix)
-    mae_val = mean_absolute_error(y_true ,y_pred)
-    mape_val = mape(y_true,y_pred,percentage_error_fix)
-    maape_val =  maape(y_true,y_pred,percentage_error_fix)
-    metric_dict = {"RMSE":rmse_val, "RMSPE": rmspe_val, "MAE":mae_val,"MAPE":mape_val, "MAAPE": maape_val}
-
-    # Set x and y labels
-    if plot : 
-        plt.figure(figsize=(28, 5))
-        plt.title('Actual vs Prediction')
-        plt.plot(y_true, label='Actuals')
-        plt.plot(y_pred, label='Predictions')
+def plot_prediction(y_true, y_pred):
+    import matplotlib.pyplot as plt
+    for i in range(len(y_pred[0,:])):
+        plt.figure(figsize=(30, 5))
+        plt.title(f'Actual vs Prediction for {i}')
+        plt.plot(y_true[:,i],label='Actuals')
+        plt.plot(y_pred[:,i], label='Predictions')
         plt.xlabel('Time')
         plt.ylabel('Value')
         plt.legend()
         plt.show()
-    return y_pred, y_true, metric_dict
+
+
+        