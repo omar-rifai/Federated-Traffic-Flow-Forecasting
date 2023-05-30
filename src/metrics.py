a2adc1f9b59fcb357274a1221341daf1c13d351a
Michael <96789995+leahcimali@users.noreply.github.com>
2023-05-30 14:33:02 +0200

    Testing (#22)
    * Add testing in the main.py
    Update variable names to be more explicit
    
    * Added the possibility of saving the plot
    
    * Correct issue #15
    
    * Change nodes params in utils_data.py
    The param is the list of nodes to process not an
    interger. So it should be a empty list as a default param.
    
    * Add metrics table to metrics.py and a test.py
    test.py is a file with a configurable to test saved model
    the metrics table is export and saved as a json and txt.
    
    * Added a test.py and export results in a txt file
    The results of main.py and test.py are exported in txt file
    The results of test.py will be exported as json
    Results are updated in real time.
    
    * Add a percentage selection of the time serie.
    You can set the parameter time_serie_percentage_length
    to a percentage if you don't want to work with the full
    time serie. The base parameter is 1.
    
    * Training in federated will only save bestmodels
    After each communication round and after each model
    averaging, the mean_squared_error validation loss of each node
    will be calculated to conserve only the best model.
    
    * Add % of superior prediction between local and fed
    Add a metric to calculate point par point superior
    prediction  between the federated models and the local model.
    Correct some unnecessary redundancy too in the code.
    
    * Correct incorrect ploting in test.py

--- src/metrics.py
+++ src/metrics.py
@@ -32,7 +32,18 @@ def rmsse(y_true, y_pred):
 
     return rmsse
 
-
+def Percentage_of_Superior_Predictions(y_true,y_pred,y_true_fed, y_pred_fed):
+    local = np.abs(y_true.flatten()-y_pred.flatten())
+    fed = np.abs(y_true_fed.flatten()-y_pred_fed.flatten())
+    comparison = np.less(fed, local).astype(int)
+    fed_better = (comparison.sum()/len(comparison))*100
+    local_better = 100*(len(comparison)-comparison.sum())/len(comparison)
+    print(
+    '''
+    The federated prediction is better {:.2f} % of the time
+    The local prediction is better {:.2f} % of the time
+    '''.format(fed_better, local_better))
+    return fed_better, local_better
 
 def calculate_metrics(y_true, y_pred,percentage_error_fix =0):
     from src.metrics import rmse, rmspe, maape, mape 
--- test.py
+++ test.py
@@ -6,7 +6,7 @@ import contextlib
 from src.utils_data import load_PeMS04_flow_data, preprocess_PeMS_data, local_dataset, plot_prediction
 from src.utils_training import train_model, testmodel
 from src.utils_fed import fed_training_plan
-from src.metrics import calculate_metrics, metrics_table
+from src.metrics import calculate_metrics, metrics_table, Percentage_of_Superior_Predictions
 import src.config
 import sys
 
@@ -61,35 +61,15 @@ with open(params.save_model_path +'test.txt', 'w') as f:
             metrics_dict[node]['local_only'] = local_metrics
 
             y_true_fed, y_pred_fed = testmodel(model(1,32,1), datadict[node]['test'], f'{params.save_model_path}bestmodel_node{node}.pth', meanstd_dict = meanstd_dict, sensor_order_list=[params.nodes_to_filter[node]])
-            fed_metric = calculate_metrics(y_true_fed, y_pred_fed)
-            metrics_dict[node][f'fed_round_{round}'] = fed_metric
+            fed_metrics = calculate_metrics(y_true_fed, y_pred_fed)
+            metrics_dict[node][f'Federated'] = fed_metrics
             print(f'Federated vs local only for node {node} :')
-            print(metrics_table({'Local' :local_metrics, f'Federated' : fed_metric }))
+            fed_metrics['Superior Pred %'], local_metrics['Superior Pred % '] = Percentage_of_Superior_Predictions(y_true, y_pred, y_true_fed, y_pred_fed)
+            print(metrics_table({'Local' :local_metrics, f'Federated' : fed_metrics }))
+            if params.plot : 
+                    plot_prediction(y_true, y_pred, datadict[node]['test_data'],meanstd_dict[params.nodes_to_filter[node]], window_size =params.window_size , time_point_t=params.time_point_to_plot, node=0, plot_fig_name = f'Localmodel_{params.num_epochs_local_no_federation}epochs_node_{node}' )
+                    plot_prediction(y_true_fed, y_pred_fed, datadict[node]['test_data'],meanstd_dict[params.nodes_to_filter[node]], window_size =params.window_size , time_point_t=params.time_point_to_plot, node=0, plot_fig_name = f'Federatedmodel_{params.num_epochs_local_federation}epochs_node_{node}_round{params.communication_rounds}' )
 
 with open(params.save_model_path + "test.json", "w") as outfile:
     json.dump(metrics_dict, outfile)
-
-for node in range(params.number_of_nodes):  
-                if params.plot : 
-                    plot_prediction(y_true, y_pred, datadict['test_data'],meanstd_dict[params.nodes_to_filter[node]], window_size =params.window_size , time_point_t=params.time_point_to_plot, node=0, plot_fig_name = f'Federated_{params.num_epochs_local_federation}epochs_node_{node}_round_{params.communication_rounds}' )
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
