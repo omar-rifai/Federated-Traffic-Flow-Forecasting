e1c00cc09e00f4e95f214a17c92a4a8d5e319a2e
Michael <michaelbenalipro@gmail.com>
2023-06-14 11:18:14 +0200

    Retrain model locally after federation Add a possibility to retrain the federated model locally on each nodes after the end of all federation experiment Removed Redundancy of ploting and testing in the main.py function since it is done in the test.py and the streamlit visual.

--- src/config.py
+++ src/config.py
@@ -17,19 +17,19 @@ class Params():
             self.center_and_reduce = config['center_and_reduce']
             self.normalize = config['normalize']
             self.sort_by_mean = config['sort_by_mean']
-            self.nodes_to_filter = config['nodes_to_filter']  #[118,168,261]
-            self.number_of_nodes =  config['number_of_nodes']  #[118,168,261]
+            self.nodes_to_filter = config['nodes_to_filter']  
+            self.number_of_nodes =  config['number_of_nodes'] 
             self.window_size = config['window_size']
             self.prediction_horizon = config['prediction_horizon']
             self.stride = config['stride']
             self.communication_rounds = config['communication_rounds']
             self.num_epochs_local_no_federation = config['num_epochs_local_no_federation']
             self.num_epochs_local_federation = config['num_epochs_local_federation']
+            self.epoch_local_retrain_after_federation = config["epoch_local_retrain_after_federation"]
             self.learning_rate = config['learning_rate']
-            self.plot = config['plot']
-            self.time_point_to_plot = config['time_point_to_plot']
             self.model = config["model"]
             self.save_model_path = config["save_model_path"]
+            
 
 
 
