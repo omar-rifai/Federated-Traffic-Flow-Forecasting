290f3de081dd507e14e250450961d1594b61dc52
Guillaume COGONI <cogoni.guillaume@gmail.com>
2023-06-15 14:05:22 +0200

    add: batch_size parameter in config.json
    Signed-off-by: Guillaume COGONI <cogoni.guillaume@gmail.com>

--- test.py
+++ test.py
@@ -51,7 +51,8 @@ with open(PATH_EXPERIMENTS +'test.txt', 'w') as f:
                                 nodes = params.nodes_to_filter,
                                 window_size=params.window_size,
                                 stride=params.stride,
-                                prediction_horizon=params.prediction_horizon)
+                                prediction_horizon=params.prediction_horizon,
+                                batch_size=params.batch_size)
         print(datadict.keys())
         metrics_dict ={}
 
@@ -76,9 +77,6 @@ with open(PATH_EXPERIMENTS +'test.txt', 'w') as f:
             print(f'Federated vs local only for node {node} :')
             fed_metrics['Superior Pred %'], local_metrics['Superior Pred %'] = Percentage_of_Superior_Predictions(y_true, y_pred, y_true_fed, y_pred_fed)
             print(metrics_table({'Local' :local_metrics, f'Federated' : fed_metrics }))
-            if params.plot :
-                    plot_prediction(y_true, y_pred, datadict[node]['test_data'],meanstd_dict[params.nodes_to_filter[node]], window_size =params.window_size , time_point_t=params.time_point_to_plot, node=0, plot_fig_name = f'Localmodel_{params.num_epochs_local_no_federation}epochs_node_{node}' )
-                    plot_prediction(y_true_fed, y_pred_fed, datadict[node]['test_data'],meanstd_dict[params.nodes_to_filter[node]], window_size =params.window_size , time_point_t=params.time_point_to_plot, node=0, plot_fig_name = f'Federatedmodel_{params.num_epochs_local_federation}epochs_node_{node}_round{params.communication_rounds}' )
 
 with open(PATH_EXPERIMENTS + "test.json", "w") as outfile:
     json.dump(metrics_dict, outfile)
