6742ecb45875313ddf5d304977ba0e3bb63bacb6
Michael <96789995+leahcimali@users.noreply.github.com>
2023-06-16 14:33:39 +0200

    Testing (#33)
    * Add a notebook to plot the multistep prediction
    
    * Retrain model locally after federation
    Add a possibility to retrain the federated model locally on each
    nodes after the end of all federation experiment
    Removed Redundancy of ploting and testing in the main.py function
    since it is done in the test.py and the streamlit visual.
    
    * Change in the main.py to work correctly
    if node_to_filter : [] in the config.json
    will replace it with the necessary nodes.

--- src/config.py
+++ src/config.py
@@ -17,8 +17,8 @@ class Params():
             self.center_and_reduce = config['center_and_reduce']
             self.normalize = config['normalize']
             self.sort_by_mean = config['sort_by_mean']
-            self.nodes_to_filter = config['nodes_to_filter']
-            self.number_of_nodes = config['number_of_nodes']
+            self.nodes_to_filter = config['nodes_to_filter']  
+            self.number_of_nodes =  config['number_of_nodes'] 
             self.window_size = config['window_size']
             self.prediction_horizon = config['prediction_horizon']
             self.stride = config['stride']
@@ -29,6 +29,7 @@ class Params():
             self.learning_rate = config['learning_rate']
             self.model = config["model"]
             self.save_model_path = config["save_model_path"]
+            
 
 
 def convert_PeMS_to_csv(flow_file="./../data/PEMS04/PEMS04.npz", csv_file="./../data/PEMS04/distance.csv"):
