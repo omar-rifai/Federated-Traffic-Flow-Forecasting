3e4cb33d42ed8f9e0b8b430e4ed3a6151f2d4bf5
Guillaume COGONI <cogoni.guillaume@gmail.com>
2023-06-19 15:48:00 +0200

    change the rendering of the 3_Map
    * Delete the visual where in function of the accuracy a circle fill
    in an other circle.
    
    Signed-off-by: Guillaume COGONI <cogoni.guillaume@gmail.com>

--- src/utils_streamlit_app.py
+++ src/utils_streamlit_app.py
@@ -127,7 +127,7 @@ def selection_of_experiment():  # sourcery skip: assign-if-exp, extract-method
             "Choose the options you want to use to filter the experiments",
             options,
             format_func=format_option,
-            default=["number_of_nodes", "prediction_horizon"]
+            default=["number_of_nodes", "prediction_horizon", "model"]
         )
 
         if (len(selected_options) > 0):
@@ -177,8 +177,8 @@ def create_circle_precision_predict(marker_location, value_percent, map_folium, 
             Hex code HTML
     """
     lat, long = marker_location
-    folium.Circle(location=[lat + 0.0020, long + 0.0018], color="black", radius=100, fill=True, opacity=1, fill_opacity=0.8, fill_color="white").add_to(map_folium)
-    folium.Circle(location=[lat + 0.0020, long + 0.0018], color=color, radius=100 * value_percent, fill=True, opacity=0, fill_opacity=1, fill_color=color).add_to(map_folium)
+    # folium.Circle(location=[lat + 0.0020, long + 0.0018], color="black", radius=100, fill=True, opacity=1, fill_opacity=0.8, fill_color="white").add_to(map_folium)
+    folium.Circle(location=[lat + 0.0020, long + 0.0018], color="black", radius=100, fill=True, opacity=1, fill_opacity=1, fill_color=color).add_to(map_folium)
     folium.map.Marker([lat + 0.0022, long + 0.0014], icon=folium.features.DivIcon(html=f"<div style='font-weight:bold; font-size: 15pt; color: black'>{int(value_percent * 100)}%</div>")).add_to(map_folium)
     # folium.Circle(location=[lat,long], color="black", radius=100, fill=True, opacity=1, fill_opacity=0.8, fill_color="white").add_to(map_folium)
     # folium.Circle(location=[lat,long], color=color, radius=100*value_percent, fill=True, opacity=0, fill_opacity=1, fill_color=color).add_to(map_folium)
