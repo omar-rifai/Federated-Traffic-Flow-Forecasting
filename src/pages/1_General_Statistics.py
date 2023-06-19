88cfdba643c18d3df789520af503b4cce6b2d330
Guillaume COGONI <cogoni.guillaume@gmail.com>
2023-06-19 15:44:29 +0200

    change: delete boxplot in 1_General_Statistics
    * Delete the box plot because it doesn't bring relevant information
    in the way it was computed.
    
    Signed-off-by: Guillaume COGONI <cogoni.guillaume@gmail.com>

--- src/pages/1_General_Statistics.py
+++ src/pages/1_General_Statistics.py
@@ -4,12 +4,35 @@
 import json
 import streamlit as st
 import pandas as pd
-import matplotlib.pyplot as plt
-import numpy as np
+
 
 from utils_streamlit_app import selection_of_experiment
 
 st.set_page_config(layout="wide")
+
+
+#######################################################################
+# Functions
+#######################################################################
+def style_dataframe(df):
+    styles = []
+    for i in range(len(df)):
+        if i % 2 == 0:
+            styles.append({
+                'selector': f'tbody tr:nth-child({i+1})',
+                'props': [('background-color', 'rgba(150, 150, 100, 0.4)', ), ('color', 'black'), ('font-weight', 'bold')],
+            })
+        else:
+            styles.append({
+                'selector': f'tbody tr:nth-child({i+1})',
+                'props': [('background-color', 'rgba(255, 150, 0, 0.4)'), ('color', 'black')],
+            })
+    styles.append({
+        'selector': 'th',
+        'props': [('font-weight', 'bold'), ('color', 'black')]
+    })
+    return styles
+
 
 #######################################################################
 # Main
@@ -41,35 +64,26 @@ if (path_experiment_selected is not None):
     metrics = list(results['0']["local_only"].keys())
     multiselect_metrics = st.multiselect('Choose your metric(s)', metrics, ["RMSE", "MAE", "SMAPE", "Superior Pred %"])
 
+    st.subheader(f"A comparison between federated and local version on {len(nodes)} sensors")
+    st.subheader("_It's a general statistic including all the sensors in the calculation_")
     if df_federated_node != []:
-        st.subheader("How much the Federated version is performant on average taking in account all the sensors for the calculation of the statistics")
         df_federated_node = pd.DataFrame(df_federated_node, columns=multiselect_metrics)
         global_stats_fed_ver = df_federated_node.describe().T
-        global_stats_fed_ver.rename(columns={'count': 'Nb sensors'}, inplace=True)
+        global_stats_fed_ver.drop(columns={'count'}, inplace=True)
         global_stats_fed_ver = global_stats_fed_ver.applymap(lambda x: '{:.2f}'.format(x))
-        st.table(global_stats_fed_ver.style.set_table_styles([{'selector': 'th', 'props': [('font-weight', 'bold'), ('color', 'black')]}]))
-
-        c1_boxplot_fed, c2_bobxplot_fed, c3_boxplot_fed = st.columns((1, 2, 1))
-        fig, ax = plt.subplots()
-        bar_plot_results = df_federated_node
-        bar_plot_results.boxplot(column=["RMSE", "MAE"], ylabel="values", xlabel="sensor", ax=ax)
-        plt.yticks(np.arange(0, max(bar_plot_results["RMSE"].max(), bar_plot_results["MAE"].max()), 10))
-        with c2_bobxplot_fed:
-            st.pyplot(fig, use_container_width=True)
 
     if df_local_node != []:
-        st.subheader("How much the Local version is performant on average taking in account all the sensors for the calculation of the statistics")
         df_local_node = pd.DataFrame(df_local_node, columns=multiselect_metrics)
         global_stats_local_ver = df_local_node.describe().T
-        global_stats_local_ver = global_stats_local_ver.apply(lambda k: k.round(0))
-        global_stats_local_ver.rename(columns={'count': 'Nb sensors'}, inplace=True)
+        global_stats_local_ver.drop(columns={'count'}, inplace=True)
         global_stats_local_ver = global_stats_local_ver.applymap(lambda x: '{:.2f}'.format(x))
-        st.table(global_stats_local_ver.style.set_table_styles([{'selector': 'th', 'props': [('font-weight', 'bold'), ('color', 'black')]}]))
 
-        c1_boxplot_local, c2_bobxplot_local, c3_boxplot_local = st.columns((1, 2, 1))
-        fig, ax = plt.subplots()
-        bar_plot_results = df_local_node
-        bar_plot_results.boxplot(column=["RMSE", "MAE"], ylabel="values", xlabel="sensor", ax=ax)
-        plt.yticks(np.arange(0, max(bar_plot_results["RMSE"].max(), bar_plot_results["MAE"].max()), 10))
-        with c2_bobxplot_local:
-            st.pyplot(fig, use_container_width=True)
+    # Create multi-level index for merging
+    common_indexes = global_stats_local_ver.index.intersection(global_stats_fed_ver.index)
+    multi_index = pd.MultiIndex.from_product([common_indexes, ['Local', 'Federated']], names=['Index', 'Version'])
+
+    merged_stats = pd.concat([global_stats_local_ver, global_stats_fed_ver], axis=0)
+
+    merged_stats.index = multi_index
+
+    st.table(merged_stats.style.set_table_styles(style_dataframe(merged_stats)))
